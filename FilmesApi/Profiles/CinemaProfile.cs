6ad80467c6d775927a4f7a445e388925d5302c80
Carmosito Neto <carmosito.neto@genialnet.com.br>
2025-12-19 18:01:29 -0300

    Criação do objeto de cinema

---
+++ FilmesApi/Profiles/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles
+{
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            CreateMap<CreateCinemaDto, CinemaProfile>();
+            CreateMap<Cinema, ReadCinemaDto>();
+            CreateMap<UpdateCinemaDto, Cinema>();
+        }
+    }
+}
